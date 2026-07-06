cfe283920566cc66dbe2d0b46273cc9187b644ea
heysiv8-collab <heysiv8@gmail.com>
2026-07-06 12:17:04 -0600

    Add files via upload

---
+++ lib/Route.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Lib;
+
+class Route
+{
+    private $routes=[];
+
+    public function get($uri, $callback)
+    {
+        $this->routes['GET'][$uri] = $callback;
+    }
+
+    public function post($uri, $callback)
+    {
+        $this->routes['POST'][$uri] = $callback;
+    }
+}
